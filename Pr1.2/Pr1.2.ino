1cdf31800e90551af2545e08111b15b5f8b5ebb3
Germangomezmartin <gomezmartingerman@yahoo.es>
2015-04-27 21:29:46 +0200

    Pr1.2

---
+++ Pr1.2/Pr1.2.ino
@@ -0,0 +1,191 @@
+const int led0 = 3;          
+const int led1 = 5;          
+const int led2 = 6;          
+const int led3 = 9;          
+const int led4 = 10;         
+const int led5 = 11;         
+int velocitat = 500;         
+
+void setup()
+{
+  pinMode(led0, OUTPUT);     
+  pinMode(led1, OUTPUT);     
+  pinMode(led2, OUTPUT);     
+  pinMode(led3, OUTPUT);     
+  pinMode(led4, OUTPUT);     
+  pinMode(led5, OUTPUT);     
+}
+
+void loop()
+{
+  analogWrite(led0, 0);      
+  analogWrite(led1, 0);      
+  analogWrite(led2, 0);     
+  analogWrite(led3, 0);      
+  analogWrite(led4, 0);     
+  analogWrite(led5, 0);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 20);      
+  analogWrite(led1, 0);      
+  analogWrite(led2, 0);     
+  analogWrite(led3, 0);      
+  analogWrite(led4, 0);     
+  analogWrite(led5, 0);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 40);      
+  analogWrite(led1, 20);      
+  analogWrite(led2, 0);     
+  analogWrite(led3, 0);      
+  analogWrite(led4, 0);     
+  analogWrite(led5, 0);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 60);      
+  analogWrite(led1, 40);      
+  analogWrite(led2, 20);     
+  analogWrite(led3, 0);      
+  analogWrite(led4, 0);     
+  analogWrite(led5, 0);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 80);      
+  analogWrite(led1, 60);      
+  analogWrite(led2, 40);     
+  analogWrite(led3, 20);      
+  analogWrite(led4, 0);     
+  analogWrite(led5, 0);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 100);      
+  analogWrite(led1, 80);      
+  analogWrite(led2, 60);     
+  analogWrite(led3, 40);      
+  analogWrite(led4, 20);     
+  analogWrite(led5, 0);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 120);      
+  analogWrite(led1, 100);      
+  analogWrite(led2, 80);     
+  analogWrite(led3, 60);      
+  analogWrite(led4, 40);     
+  analogWrite(led5, 20);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 140);      
+  analogWrite(led1, 120);      
+  analogWrite(led2, 100);     
+  analogWrite(led3, 80);      
+  analogWrite(led4, 60);     
+  analogWrite(led5, 40);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 160);      
+  analogWrite(led1, 140);      
+  analogWrite(led2, 120);     
+  analogWrite(led3, 100);      
+  analogWrite(led4, 80);     
+  analogWrite(led5, 60);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 180);      
+  analogWrite(led1, 160);      
+  analogWrite(led2, 140);     
+  analogWrite(led3, 120);      
+  analogWrite(led4, 100);     
+  analogWrite(led5, 80);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 200);      
+  analogWrite(led1, 180);      
+  analogWrite(led2, 160);     
+  analogWrite(led3, 140);      
+  analogWrite(led4, 120);     
+  analogWrite(led5, 100);
+  
+  delay(velocitat);           
+          
+  analogWrite(led0, 220);      
+  analogWrite(led1, 200);      
+  analogWrite(led2, 180);     
+  analogWrite(led3, 160);      
+  analogWrite(led4, 140);     
+  analogWrite(led5, 120);
+  
+  delay(velocitat);
+  
+  analogWrite(led0, 240);      
+  analogWrite(led1, 220);      
+  analogWrite(led2, 200);     
+  analogWrite(led3, 180);      
+  analogWrite(led4, 160);     
+  analogWrite(led5, 140);
+  
+  delay(velocitat);
+  
+  analogWrite(led0, 255);      
+  analogWrite(led1, 240);      
+  analogWrite(led2, 220);     
+  analogWrite(led3, 200);      
+  analogWrite(led4, 180);     
+  analogWrite(led5, 160);
+  
+  delay(velocitat);
+  
+  analogWrite(led0, 255);      
+  analogWrite(led1, 255);      
+  analogWrite(led2, 240);     
+  analogWrite(led3, 220);      
+  analogWrite(led4, 200);     
+  analogWrite(led5, 180);
+  
+  delay(velocitat);
+  
+  analogWrite(led0, 255);      
+  analogWrite(led1, 255);      
+  analogWrite(led2, 255);     
+  analogWrite(led3, 240);      
+  analogWrite(led4, 220);     
+  analogWrite(led5, 200);
+  
+  delay(velocitat);
+  
+  analogWrite(led0, 255);      
+  analogWrite(led1, 255);      
+  analogWrite(led2, 255);     
+  analogWrite(led3, 255);      
+  analogWrite(led4, 240);     
+  analogWrite(led5, 220);     
+
+  delay(velocitat);           
+  
+  analogWrite(led0, 255);      
+  analogWrite(led1, 255);      
+  analogWrite(led2, 255);     
+  analogWrite(led3, 255);      
+  analogWrite(led4, 255);     
+  analogWrite(led5, 240);     
+
+  delay(velocitat);
+  
+  analogWrite(led0, 255);      
+  analogWrite(led1, 255);      
+  analogWrite(led2, 255);     
+  analogWrite(led3, 255);      
+  analogWrite(led4, 255);     
+  analogWrite(led5, 255);     
+
+  delay(velocitat);             
+}
